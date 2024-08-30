92cc533697c03feb334c1cba4ec197b266287797
MilaGallegos <milagallegos29@gmail.com>
2024-08-30 14:53:37 -0600

    Create numero pares.c

---
+++ numero pares.c
@@ -0,0 +1,16 @@
+#include ‹stdio.h>
+#include‹math.h>
+int i;
+int main ()
+{
+  printf（"Los numeros pares entre 0 y 100 son");
+for （i=0; i <=100; i++）
+{
+  if (i % 2 == 0)
+  {
+    printf(" %d", i);
+  ｝
+}
+  printf("\ก"):
+  return 0;
+}
